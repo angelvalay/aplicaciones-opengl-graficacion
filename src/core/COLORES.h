df3974ddd5d1477612e138036adbbd4de0fc1e58
Angel Valay <angelvalay@gmail.com>
2019-09-27 01:42:54 -0500

    anadir archivos principales

---
+++ src/core/COLORES.h
@@ -0,0 +1,19 @@
+//
+// Created by angel on 27/09/19.
+//
+
+#ifndef PRACTICAS_COLORES_H
+#define PRACTICAS_COLORES_H
+
+struct ColorRGB{
+    double Rojo;
+    double Verde;
+    double Azul;
+};
+
+const ColorRGB COLOR_ROJO{1,0,0};
+const ColorRGB COLOR_BLANCO{0,0,0};
+const ColorRGB COLOR_AZUL{0,0,1};
+const ColorRGB COLOR_VERDE{0,1,0};
+
+#endif //PRACTICAS_COLORES_H
---
+++ src/core/LIBRERIAS.h
@@ -0,0 +1,14 @@
+//
+// Created by angel on 27/09/19.
+//
+
+#ifndef PRACTICAS_LIBRERIAS_H
+#define PRACTICAS_LIBRERIAS_H
+
+#ifdef __APPLE__
+#  include <GLUT/glut.h>
+#else
+#  include <GL/glut.h>
+#endif
+
+#endif //PRACTICAS_LIBRERIAS_H
