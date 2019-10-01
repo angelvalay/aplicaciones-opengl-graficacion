8dd4279d782fa2f4cc44d80cb12323f7ca247eff
Angel Valay <angelvalay@gmail.com>
2019-09-30 23:30:58 -0500

    correciones de la practica 1

--- src/practica_1/Aplicacion.cpp
+++ src/practica_1/Aplicacion.cpp
@@ -2,27 +2,53 @@
 // Created by angel on 27/09/19.
 //
 
-#define AlturaVentana 700
-#define AnchuraVentana 1000
+#define AlturaVentana 700 // Defino la altura predeterminada de la pantalla
+#define AnchuraVentana 1000 // Defino la anchura predeterminada de la pantalla
 
-#define UMBRAL 0.00000001
+#define UMBRAL 0.00000001 //Umbral necesaria para determinar la separacion de lineas
 
-double colVen = AnchuraVentana / (24);
-double filaVen = AlturaVentana /(16);
+double colVen = AnchuraVentana / (24); // Valor de los pixeles que contiene cada columna
+double filaVen = AlturaVentana /(16); // Pixeles que contiene cada fila
 
-
-// Routine to draw a stroke character string.
+/**
+ * Funcion para escribir texto como si fuera un conjunto de lineas
+ * @param font Tipo de fuente
+ * @param string Cadena de texto que se quiere mostrar
+ */
 void escribirTexto(void *font, char *string)
 {
     char *c;
     for (c = string; *c != '\0'; c++) glutStrokeCharacter(font, *c);
 }
-void writeBitmapString(void *font, char *string)
+/**
+ * Funcion para escribir texto bitmap
+ * @param font Tipo de fuente
+ * @param string Cadena de texto que se quiere mostrar
+ */
+void escribirTextoBitMap(void *font, char *string)
 {
     char *c;
     for (c = string; *c != '\0'; c++) glutBitmapCharacter(font, *c);
 }
 
+void mostrarInfoPractica(){
+    glColor3f(0.0, 0.0, 0.0);
+    glRasterPos3f(18*colVen, 1.8*filaVen, 0.0);
+    escribirTextoBitMap(GLUT_BITMAP_8_BY_13, "Angel Armando Valay Martinez");
+    glRasterPos3f(18*colVen, 1.4*filaVen, 0.0);
+    escribirTextoBitMap(GLUT_BITMAP_8_BY_13, "1730322");
+    glRasterPos3f(18*colVen, 1.0*filaVen, 0.0);
+    escribirTextoBitMap(GLUT_BITMAP_8_BY_13, "INTRODUCCION A LA GRAFICACION ");
+    glRasterPos3f(18*colVen, 0.6*filaVen, 0.0);
+    escribirTextoBitMap(GLUT_BITMAP_8_BY_13, "POR COMPUTADORA");
+}
+
+/**
+ * Funcion para generar la estructura inicial de la pantalla
+ * @param argc Numero de argumentos de la app
+ * @param argv Array de los argumentos
+ * @param titulo Cadena de texto del titulo
+ */
 void generarPantalla(int argc, char **argv, char* titulo) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
@@ -30,6 +56,11 @@ void generarPantalla(int argc, char **argv, char* titulo) {
     glutInitWindowPosition(100, 100);
     glutCreateWindow(titulo);
 }
+/**
+ * Funcion para redimensionar la pantalla
+ * @param width Anchura predeterminada
+ * @param height Altura predeterminada
+ */
 void redimensionarPantalla(int width, int height) {
     glViewport(0, 0, (GLsizei)width, (GLsizei)height);
     glMatrixMode(GL_PROJECTION);
@@ -38,70 +69,73 @@ void redimensionarPantalla(int width, int height) {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
-void onKeyInput(unsigned char key, int x, int y) {
-    switch(key)
-    {
-        default:
-            break;
-    }
-}
-void activarFunciones() {
+/**
+ * Funcion para iniciarlizar la configuracion del programa
+ */
+void iniciarConfiguracion() {
     glClearColor(COLOR_BLANCO.Rojo, COLOR_BLANCO.Verde, COLOR_BLANCO.Azul, 0.0);
     glutMainLoop();
 }
 
 /**
- * FUNCIONES PARA LA PRACTICA
+ * Funcion para generar puntos en la grafica
+ * @param puntos COnjunto de puntos para generar
  */
-void generarPuntos(std::vector<Punto> puntos){
+void generarPuntos(vector<Punto>& puntos){
     glColor3f(COLOR_ROJO.Rojo, COLOR_ROJO.Verde, COLOR_ROJO.Azul);
     glPointSize(5);
     glBegin(GL_POINTS);
-        for (auto i = puntos.begin(); i != puntos.end(); ++i){
-            cout<<(*i).x <<" "<<(*i).y<< " "<<(*i).z << " "<<endl;
-            glVertex3f((*i).x, (*i).y, (*i).z);
+        for (Punto & punto : puntos){
+            glVertex3f(punto.x, punto.y, punto.z);
         }
     glEnd();
+    glEnable(GL_PROGRAM_POINT_SIZE_EXT); // permite reiniciar el tamano del punto
+    glPointSize(1);
 }
-
+/**
+ * Funcion para generar el cuadro de la grafica lineal en donde se
+ * contendra la grafica lineal de acuerdo a los datos obtenidos
+ */
 void generarCuadro(){
     glColor3f(0.7, 0.7, 0.7);
     //cuadro de la grafica
     glBegin(GL_POLYGON);
         glVertex3f(3*colVen, 3*filaVen, 0.0);
         glVertex3f(24*colVen, 3*filaVen, 0.0);
-        glVertex3f(24*colVen, 16*filaVen, 0.0);
-        glVertex3f(3*colVen, 16*filaVen, 0.0);
+        glVertex3f(24*colVen, 15*filaVen, 0.0);
+        glVertex3f(3*colVen, 15*filaVen, 0.0);
     glEnd();
 }
-
+/**
+ * FUncion para generar las lineas que enmarcan el cuadro de la grafica
+ */
 void generarLineas(){
     glColor3f(0.5, 0.5, 0.5);
-//    glPointSize(100.0);
-//    glBegin(GL_LINES);
-//     glBegin(GL_LINE_STRIP);
-
     glBegin(GL_LINE_LOOP);
         glVertex3f(3*colVen, 3*filaVen, 0.0);
         glVertex3f(24*colVen, 3*filaVen, 0.0);
-        glVertex3f(24*colVen, 16*filaVen, 0.0);
-        glVertex3f(3*colVen, 16*filaVen, 0.0);
+        glVertex3f(24*colVen, 15*filaVen, 0.0);
+        glVertex3f(3*colVen, 15*filaVen, 0.0);
     glEnd();
 
 }
-
+/**
+ * Funcion para generar las lineas punteadas sobre el eje Y
+ * @param min Valor minimo que contiene la grafica
+ * @param max VAlor maximo que contiene la grafica
+ * @param umbral La diferencia que existe entre cada linea que sera dibujada
+ * @param dif Valor diferencial entre el maximo y minimo
+ */
 void generarLineasYGrafico(double min, double max, double umbral, double dif){
-    double difFila = 13/dif;
+    double difFila = 12/dif;
     double contFila = 0;
-//    cout<<difCol*filaVen<<" "<<dif<<endl;
     glColor3f(0.5, 0.5, 0.5);
+    //Aqui se empiezan a dibujar las lineas punteadas
     glEnable(GL_LINE_STIPPLE);
     glLineStipple(1, 0x00FF);
     glBegin(GL_LINES);
         for (double i = min; i < max;) {
             if(fmod(i,umbral) == 0) {
-//                cout<<"\t"<<i<<"\t";
-//                cout<<contCol<<" "<<contCol*difCol<<endl;
                 glVertex3f(3*colVen, 3*filaVen + (contFila*difFila*filaVen), 0.0);
                 glVertex3f(24*colVen, 3*filaVen + (contFila*difFila*filaVen), 0.0);
             }
@@ -112,74 +146,88 @@ void generarLineasYGrafico(double min, double max, double umbral, double dif){
     glDisable(GL_LINE_STIPPLE);
     contFila = 0;
     glColor3f(0.0, 0.0, 0.0);
+    //Aqui se empieza a anadir el texto que representa el valor de cada linea punteada
     for (double i = min; i < max;) {
         if(fmod(i,umbral) == 0) {
-//                cout<<"\t"<<i<<"\t";
-//                cout<<contCol<<" "<<contCol*difCol<<endl;
             glRasterPos3f(2*colVen, 3*filaVen + (contFila*difFila*filaVen), 0.0);
             char* string = const_cast<char *>(to_string((int)i).c_str());
-            writeBitmapString(GLUT_BITMAP_8_BY_13, string);
+            escribirTextoBitMap(GLUT_BITMAP_8_BY_13, string);
         }
         i+=1;
         contFila++;
     }
 }
-
-void generarPuntosGrafico(double min, double max){
-//    cout<<datos.size()<<endl;
-    double difFila = 13/(max-min);
+/**
+ * Funcion para generar los puntos y lineas de la grafica lineal y texto en el eje X
+ * @param min Valor minimo en funcion para el eje Y
+ * @param max Valor maximo en funcion para el eje Y
+ * @param size Cantidad de puntos que se agregaran a la grafica
+ */
+void generarPuntosGrafico(double min, double max, int size){
+    double difFila = 12/(max-min);
     double contFila = 0;
-    double difCol = 21/(datos.size());
-    cout<<difCol*colVen<<" "<<datos.size()+1<<endl;
+    double difCol = 21.00/(size+1);
     int contCol = 1;
     glColor3f(0.0, 0.0, 0.0);
-    for(Dato& val:datos){
-        cout<<contCol<<endl;
+    //Aqui empieza la generacion de texto en el eje X
+    for (int i = 0; i < size; ++i) {
         glRasterPos3f(3*colVen + (contCol*difCol*colVen), 2.5*filaVen , 0.0);
-        char* string = const_cast<char *>(val.nombre.c_str());
-        writeBitmapString(GLUT_BITMAP_8_BY_13, string);
+        char* string = const_cast<char *>(datos[i].nombre.c_str());
+        escribirTextoBitMap(GLUT_BITMAP_8_BY_13, string);
         contCol++;
     }
     contCol = 1;
     glColor3f(COLOR_ROJO.Rojo, COLOR_ROJO.Verde, COLOR_ROJO.Azul);
+    // AQui empieza la generacion de lineas para la grafica lineal
     glBegin(GL_LINE_STRIP);
-    for (auto & dato : datos){
+    for (Dato & dato : datos){
         contFila = 0;
         for (double j = min; j < max;) {
             if(j == dato.valor){
                 glVertex3f(3*colVen + (contCol*difCol*colVen), 3*filaVen + (contFila*difFila*filaVen), 0.0);
-//                glVertex3f(3*colVen + (contCol*difCol*colVen), 5*filaVen, 0.0);
+                //AQui se van guardando los puntos que se van generando al vector puntos
+                puntos.push_back(Punto{
+                        3*colVen + (contCol*difCol*colVen),
+                        3*filaVen + (contFila*difFila*filaVen)
+                });
             }
             contFila ++;
             j+=1;
         }
-
         contCol++;
 
     }
     glEnd();
 }
-
+/**
+ * FUncion para la generacion de los texto del EJe X, Y y el titulo.
+ */
 void generarTextosPrimarios(){
     glColor3f(0.0, 0.0, 0.0);
+    // Texto titulo
+    glRasterPos3f(7*colVen , 15.5*filaVen , 0.0);
+    char* string = const_cast<char *>(tituloVentana.c_str());
+    escribirTextoBitMap(GLUT_BITMAP_TIMES_ROMAN_24, string);
+
     //Texto Y
     glPushMatrix();
         glTranslatef(colVen, 7*filaVen, 0.0);
         glRotatef(90.0, 0.0, 0.0, 1.0);
         glScalef(10/colVen, 10/filaVen, 0.001);
-        escribirTexto(GLUT_STROKE_ROMAN, "TITULO Y");
+        escribirTexto(GLUT_STROKE_ROMAN, const_cast<char *>(tituloY.c_str()));
     glPopMatrix();
     //Texto X
     glPushMatrix();
     glTranslatef(7*colVen, filaVen, 0.0);
     glRotatef(0.0, 0.0, 0.0, 1.0);
     glScalef(10/colVen, 10/filaVen, 0.001);
-    escribirTexto(GLUT_STROKE_ROMAN, "TITULO X");
+    escribirTexto(GLUT_STROKE_ROMAN, const_cast<char *>(tituloX.c_str()));
     glPopMatrix();
 }
-
-//Para agregar los textos de acuerdo a los datos que hay.
-void generarDescripcionGrafica(){
+/**
+ * FUncion para generar el cuerpo de la grafica lineal
+ */
+void generarGrafica(){
     // datos para la grafica
     double minValor = DBL_MAX;
     double maxValor = DBL_MIN;
@@ -194,9 +242,11 @@ void generarDescripcionGrafica(){
             maxValor = val.valor;
         contador++;
     }
+    // se guarda la diferencia que existe entre los extremos
     diferencia = maxValor - minValor;
     diferencia = (diferencia<0)?((-1)*diferencia):diferencia;
-//    cout<<" "<<minValor <<" "<<maxValor <<" "<<diferencia<<" "<<contador<<endl;
+    // con estos calculos se permite dar un valor aproximado sobre
+    // cuanta separacion tendran las lineas punteadas
     double umbral = UMBRAL;
     long int dec = 0;
     do{
@@ -205,20 +255,24 @@ void generarDescripcionGrafica(){
             break;
         umbral*=10;
     }while(true);
-
+    // Llamar a la funcion para generar las lineas punteadas y textos en el EJE Y
     generarLineasYGrafico(minValor-umbral,
             maxValor+umbral,umbral,diferencia+(umbral*2));
-    generarPuntosGrafico(minValor-umbral,maxValor+umbral);
+    // llamar a la funcion para generar las lineas y datos en el eje X
+    generarPuntosGrafico(minValor-umbral,maxValor+umbral, contador);
 }
 
+/**
+ * Funcion principal que permite dibujar los graficos
+ */
 void dibujarPantalla() {
     glClear(GL_COLOR_BUFFER_BIT);
-
     generarCuadro();
     generarLineas();
     generarTextosPrimarios();
-    generarDescripcionGrafica();
-
+    generarGrafica();
+    generarPuntos(puntos);
+    mostrarInfoPractica();
     glFlush();
 }
 
--- src/practica_1/main.cpp
+++ src/practica_1/main.cpp
@@ -1,40 +1,118 @@
+/**
+ * IMPORTACION DE LIBRERIAS NATIVAS
+ */
 #import <vector>
 #include <iostream>
 #include <map>
 #include <cfloat>
-#include <tgmath.h>
+#include <fstream>
+#include <sstream>
+#include <cmath>
 
+// USANDO LA ABREVIATURA DEL std::...
 using namespace std;
 
+/**
+ * IMPORTACION DE ARCHIVOS NECESARIOS PARA LA EJECUCION DEL PROGRAMA
+ */
 #import "../core/Librerias.h"
 #import "../core/Colores.h"
 #import "../core/Estructuras.h"
 
+/**
+ * estructura para almacenar los datos necesarios para la grafica
+ */
 struct Dato{
-    std::string nombre;
-    double valor;
+    std::string nombre; // nombre del correspondiente dato de entrada
+    double valor; // valor numerico
 };
-vector<Dato> datos;
 
-#import "Aplicacion.cpp"
+vector<Dato> datos; // Vector de tipo dato
+string tituloX = "Valores X"; // titulo para la coordenada X
+string tituloY = "Valores Y"; // titulo para la coordenada Y
+string tituloVentana = "titulo"; // titulo para la grafica
+vector<Punto> puntos; // conjunto de puntos de la grafica
+
+#import "Aplicacion.cpp" //importar las demas funciones que se utilizaron
+
+/**
+ * Function para leer los datos de un archivo CSV
+ * @return un vector de tipo dato que contiene los datos necesarios para mostrar la grafica
+ */
+vector<Dato> leer_csv(string nombre_archivo)
+{
+    vector<Dato> datos_csv; //Vector temporal para agregar los datos.
+    fstream fin; // Archivo CSV
+
+    int cont_col = 0; // contador de columnas
+    int cont_fila = 0; // contador de filas
+
+    fin.open(nombre_archivo,
+            ios::in); // Para generar un enlace de tipo lectura al archivo
+
+    string linea; // Sirve para guardar temporalmente cada linea del archivo
+
+    /**
+     * Ciclo para leer el archivo CSV
+     */
+    while (getline(fin,linea)) {
+        string _primerdato; // para guardar el datos de la 1era columna
+        double _segundodato = 0.0; // para guardar el datos de la 2da columna
+        string palabra; // Para guardar temporalmente la palabra que valla extrayendo por fila
+
+        //Creamos un stream para usar las funciones propias del objecto en lugar de manuparlo como un string
+        stringstream linea_auxiliar(linea);
+
+        /**
+         * Ciclo para obtener los valores por columna de la fila
+         * Solo es necesario obtener los primeros dos columnas
+         */
+        while (getline(linea_auxiliar, palabra, ',')) {
+//            cout << palabra<<"\t";
+            if (cont_col ==0) // si es la 1era columna
+                if (!cont_fila) // si la fila de los datos es la primera es encabezado
+                    tituloX = palabra; // se guarda como el titulo para el eje X
+                else
+                    _primerdato = palabra; // se guarda el dato el vector
+            else
+            if (cont_col == 1)// si es la 2da columna
+                if (!cont_fila) // si la fila de los datos es la primera es encabezado
+                    tituloY = palabra; // se guarda como el titulo para el eje Y
+                else
+                    _segundodato = atof(palabra.c_str()); // se guarda el dato el vector
+            cont_col++; // se incrementa el contador para las columnas
+        }
+//        cout<<endl;
+        cont_col = 0; // se reinicia el contador de las columnas cada que cambia de fila
+        if (cont_fila) // si se registro una fila (no encabezado) al vector
+            datos_csv.push_back(Dato{_primerdato,_segundodato}); // guardar los valores de las variables
+        cont_fila ++; // incrementa el contador de filas
+    }
+    return datos_csv; // returna los datos del csv
+}
+
 
 int main(int argc, char **argv)
 {
-    datos.push_back(Dato{"1970",15});
-    datos.push_back(Dato{"1980",30});
-    datos.push_back(Dato{"1990",60});
-    datos.push_back(Dato{"2000",120});
-    datos.push_back(Dato{"2010",240});
-    datos.push_back(Dato{"2014",300});
-//    datos.push_back(Dato{"Siete",-87});
-//    datos.push_back(Dato{"Ocho",-87});
-//    datos.push_back(Dato{"Nueve",-8});
-//    datos.push_back(Dato{"Diez",-80});
-
+    // Se valida que exactamente tenga como entrada el archivo fuente de datos
+//    if (argc != 2){
+//        cerr<<"./app nombre_archivo.csv"<<endl;
+//        cerr<<"No se ingreso un archivo de entrada como argumento, intentenlo de nuevo"<<endl;
+//        exit(0);
+//    }
+    //Llama a la funcion para obtener los datos con el argumento del archivo
+    datos = leer_csv("/home/angel/PycharmProjects/practicas-graficacion/school_year.csv");
+    //Se pide el titulo para la grafica
+//    cout<<"Ingrese un titulo para la grafica que sera mostrado en la aplicacion"<<endl;
+//    getline(cin,tituloVentana);
+    tituloVentana = "Practica 1 | Grafica lineal";
+    // Para generar las configuraciones iniciales de la ventana
     generarPantalla(argc, argv,"Practica 1 | Grafica lineal");
+    // Funcion para dibujar en pantalla los graficos necesarios
     glutDisplayFunc(dibujarPantalla);
+    // Funcion para redimensionar la pantalla
     glutReshapeFunc(redimensionarPantalla);
-    glutKeyboardFunc(onKeyInput);
-    activarFunciones();
+    // Funcion para iniciar la configuracion
+    iniciarConfiguracion();
     return 0;
 }
